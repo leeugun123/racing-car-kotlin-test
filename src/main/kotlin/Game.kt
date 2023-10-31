9e3ee1435476e75d9f40fa32bbe8a7482fcb3f12
leeugun123 <leeugun123@naver.com>
2023-10-31 22:38:29 +0900

    초기 세팅 기능 분리

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,5 +1,3 @@
-import camp.nextstep.edu.missionutils.Console
-
 class Game {
 
     companion object {
@@ -10,7 +8,7 @@ class Game {
     fun start(){
 
         //1. 초기 설정
-        initSet()
+        InitSetting.initSet()
 
 
         //2. 카 시뮬레이션 실행
@@ -30,61 +28,7 @@ class Game {
 
     }
 
-    fun initSet(){
 
-        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-
-        //2. 초기 설정
-        val names  = Console.readLine()
-
-        if (names == null || "," !in names) {
-            throwException()
-        }//쉼표가 없을 경우
-        else
-            setCarList(names)
-
-        println("시도할 횟수는 몇 회인가요?")
-
-        val count = Console.readLine()
-
-        if(count == null || count.toIntOrNull() == null)
-            throwException()
-        else
-            carList.setCount(count.toInt())
-
-
-    }
-
-
-
-    private fun setCarList(names: String?) {
-
-        val nameList = names!!.split(",").map { it.trim() }
-
-        for (name in nameList) {
-
-            if(checkName(name)){
-                carList.addCar(name)
-            }
-            else
-                throwException()
-
-        }
-        //5자 이하가 아닌 경우 예외처리
-
-    }
-
-    private fun checkName(name: String): Boolean {
-
-        return name.length <= 5
-
-    }
-
-    private fun throwException(){
-
-        throw IllegalArgumentException("")
-
-    }
 
 
 
