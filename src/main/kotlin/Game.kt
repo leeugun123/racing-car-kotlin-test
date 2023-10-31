f4bd35def3d8c4801965b7ed8cd0d8fab46b21dd
leeugun123 <leeugun123@naver.com>
2023-10-31 20:34:09 +0900

    Game 클래스 구현 - 올바른 이름인지 확인하는 메소드 구현

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,7 +2,8 @@ import camp.nextstep.edu.missionutils.Console
 
 class Game {
 
- var carList : CarList = CarList(mutableListOf<Car>() , 0)
+    var carList : CarList = CarList(mutableListOf<Car>() , 0)
+
     fun start(){
 
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
@@ -10,9 +11,36 @@ class Game {
         val names  = Console.readLine()
 
 
+    }
+
+    private fun setCarList(names: String?) {
+
+        val nameList = names!!.split(",").map { it.trim() }
+
+        for (name in nameList) {
+
+            if(checkName(name)){
+
+            }
+            else
+                throwException()
+
+        }
 
 
     }
+
+    private fun checkName(name: String): Boolean {
+
+        return name.length <= 5
+    }
+
+    private fun throwException(){
+
+        throw IllegalArgumentException("올바른 형식으로 이름을 입력해주세요.")
+
+    }
+
 
 
 
